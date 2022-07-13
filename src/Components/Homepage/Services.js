9ba9ba00825fb60bb1e8e4af18012c41cf9d0861
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-07-14 02:04:58 +0530

    Final Commit

--- src/Components/Homepage/Services.js
+++ src/Components/Homepage/Services.js
@@ -14,63 +14,63 @@ function Services() {
             </div>
             <div className="px-xxl-5 pt-5">
                 <div className="row  px-xxl-5">
-                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4">
-                        <div className="card px-5 pt-3" style={{ border: "none" }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4 flex-container">
+                        <div className="card px-3 pt-3 w-100" style={{ border: "none" }}>
                             <img src="https://ih1.redbubble.net/image.2891202113.1504/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="#image not found" width="70px" height="70px" />
                             <div className="card-body">
                                 <h5 className="card-title">window maintenance</h5>
                                 <p className="card-text">Fames velit molestias facilisi aene an, sunt, aenean maiores facilisi, artu ent adipisc ing architecto mi frin</p>
-                                <button type="button" className="btn btn-danger service_order_btn" style={{ float: "right" }}>Order Now</button>
+                                <br /><button type="button" className="btn btn-danger service_order_btn" style={{ right:"10px", bottom: "10px", position: "absolute" }}>Order Now</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4">
-                        <div className="card px-5 pt-3" style={{ border: "none" }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4 flex-container">
+                        <div className="card px-3 pt-3 w-100" style={{ border: "none" }}>
                             <img src="https://ih1.redbubble.net/image.2891202113.1504/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="#image not found" width="70px" height="70px" />
                             <div className="card-body">
                                 <h5 className="card-title">roof maintenance</h5>
                                 <p className="card-text">inspect entire roof for any water leakage penetrations, services includes roof caulkin for any exposed nails, 2 pipe collards, and any waterproofing</p>
-                                <button type="button" className="btn btn-danger service_order_btn" style={{ float: "right" }}>Order Now -$750</button>
+                                <br /><button type="button" className="btn btn-danger service_order_btn" style={{ right:"10px", bottom: "10px", position: "absolute" }}>Order Now -$750</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4">
-                        <div className="card px-5 pt-3" style={{ border: "none" }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4 flex-container">
+                        <div className="card px-3 pt-3 w-100" style={{ border: "none" }}>
                             <img src="https://ih1.redbubble.net/image.2891202113.1504/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="#image not found" width="70px" height="70px" />
                             <div className="card-body">
                                 <h5 className="card-title">Gutter Cleaning</h5>
                                 <p className="card-text">clean the entire Gutter system, water hose the downspouts, install Gutter Guard Filters, up to 100 Ift of gutters.</p>
-                                <button type="button" className="btn btn-danger service_order_btn" style={{ float: "right" }}>Order Now -$250</button>
+                                <br /><button type="button" className="btn btn-danger service_order_btn" style={{ right:"10px", bottom: "10px", position: "absolute" }}>Order Now -$250</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4">
-                        <div className="card px-5 pt-3" style={{ border: "none" }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4 flex-container">
+                        <div className="card px-3 pt-3 w-100" style={{ border: "none" }}>
                             <img src="https://ih1.redbubble.net/image.2891202113.1504/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="#image not found" width="70px" height="70px" />
                             <div className="card-body">
                                 <h5 className="card-title">window screen replacement</h5>
                                 <p className="card-text">Replace up to 5 window screens, at a price of $85 per screen, a minimum of 5 window screens.</p>
-                                <button type="button" className="btn btn-danger service_order_btn" style={{ float: "right" }}>Order Now -$425</button>
+                                <br /><button type="button" className="btn btn-danger service_order_btn" style={{ right:"10px", bottom: "10px", position: "absolute" }}>Order Now -$425</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4">
-                        <div className="card px-5 pt-3" style={{ border: "none" }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4 flex-container">
+                        <div className="card px-3 pt-3 w-100" style={{ border: "none" }}>
                             <img src="https://ih1.redbubble.net/image.2891202113.1504/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="#image not found" width="70px" height="70px" />
                             <div className="card-body">
                                 <h5 className="card-title">powerwashing</h5>
                                 <p className="card-text">powerwash</p>
-                                <button type="button" className="btn btn-danger service_order_btn" style={{ float: "right" }}>Order Now</button>
+                                <br /><button type="button" className="btn btn-danger service_order_btn" style={{ right:"10px", bottom: "10px", position: "absolute" }}>Order Now</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4">
-                        <div className="card px-5 pt-3" style={{ border: "none" }}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 pt-4 flex-container">
+                        <div className="card px-3 pt-3 w-100" style={{ border: "none" }}>
                             <img src="https://ih1.redbubble.net/image.2891202113.1504/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="#image not found" width="70px" height="70px" />
                             <div className="card-body">
                                 <h5 className="card-title">alluminum fascia boards</h5>
                                 <p className="card-text">install 30 linear feet of alluminum cap fascia board, a minimum of 30 linear feet $15 per foot after that. color does not determine the price.</p>
-                                <button type="button" className="btn btn-danger service_order_btn" style={{ float: "right" }}>Order Now -$540</button>
+                                <br /><button type="button" className="btn btn-danger service_order_btn" style={{ right:"10px", bottom: "10px", position: "absolute" }}>Order Now -$540</button>
                             </div>
                         </div>
                     </div>
