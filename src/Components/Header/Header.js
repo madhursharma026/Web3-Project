82bc7829f1d3aa0d852f0933244c94980efc43ba
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-07-14 01:22:58 +0530

    Initial Commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <>
             <nav className="navbar fixed-top" style={{backgroundColor:"#e3f2fd"}}>
                 <div className="container-md padding_left_after_md_scrn">
-                    <Link className="navbar-brand" to="/">Web3 Projects</Link>
+                    <Link className="navbar-brand" to="/"><img src="https://madhur-hq.monday.com/protected_static/12766587/resources/543146068/big-18CFB641-8F2D-4506-A856-E385B030E2B5.jpeg" alt="#" height='50px' /></Link>
                     <div className="d-flex">
                     <Link to="/login" className="btn btn-danger">Login</Link>
                     </div>
