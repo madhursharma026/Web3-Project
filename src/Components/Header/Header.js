e33e95f44a0f7740d5a2fa6012992e13e2f95277
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-07-14 13:04:50 +0530

    Update Metamask

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,13 +4,30 @@ import HeaderLogo from '../../Assets/HeaderLogo/HeaderLogo.jpeg';
 
 
 function Header() {
+
+    async function getAccount(){
+        const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+        const account = accounts[0];
+        return account
+    }
+
+    function connectButtonOnClick(){
+        if (typeof window !== 'undefined'){
+            getAccount().then((response) => {
+                console.log(response)
+            }).catch(() => console.log("User didn't connect with metamask"))
+        }
+    }
+
+
     return (
         <>
             <nav className="navbar fixed-top" style={{ backgroundColor: "black" }}>
                 <div className="container-md padding_left_after_md_scrn">
                     <Link className="navbar-brand p-0" to="/"><img className='header_logo' src={HeaderLogo} alt="#" /></Link>
                     <div className="d-flex">
-                        <Link to="/login" className="btn btn-danger">Login</Link>
+                        {/* <Link to="/login" className="btn btn-danger">Login</Link> */}
+                        <button type="button" className="btn btn-danger" onClick={() => connectButtonOnClick()}>Login</button>
                     </div>
                 </div>
             </nav>
@@ -19,3 +36,4 @@ function Header() {
 }
 
 export default Header;
+
